722563ecd890a87a7d4bd46424af05ec1177ca5e
Mahesh Paliath <mahesh.paliath@live.com>
2025-06-01 12:35:37 -0700

    Add card component from shadcn

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,10 @@ export default defineConfig({
       '/api': 'http://localhost:3000'
     }
   },
-  build: { outDir: "dist/client" }
+  build: { outDir: "dist/client" },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),   //  <-- important line
+    },
+  },
 });
